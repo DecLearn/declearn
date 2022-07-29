f46c16259389c0edf2014db867ec9ea05b5e8d32
Paul Andrey <paul.andrey@inria.fr>
2022-07-29 13:27:04 +0200

    Add full support for `tf.IndexedSlices` in `TensorflowVector`.

--- declearn2/model/tensorflow/_vector.py
+++ declearn2/model/tensorflow/_vector.py
@@ -3,9 +3,8 @@
 """TensorflowVector gradients container."""
 
 import json
-from typing import Any, Callable, Dict, Optional
+from typing import Any, Callable, Dict, Optional, Tuple, Union
 
-import numpy as np
 import tensorflow as tf  # type: ignore
 # false-positive; pylint: disable=no-name-in-module
 from tensorflow.python.framework.ops import EagerTensor  # type: ignore
@@ -15,20 +14,26 @@ from declearn2.model.api import NumpyVector, Vector, register_vector_type
 from declearn2.utils import deserialize_numpy, serialize_numpy
 
 
-@register_vector_type(tf.Tensor, EagerTensor)
+@register_vector_type(tf.Tensor, EagerTensor, tf.IndexedSlices)
 class TensorflowVector(Vector):
     """Vector subclass to store tensorflow tensors.
 
     This Vector is designed to store a collection of named
     TensorFlow tensors, enabling computations that are either
     applied to each and every coefficient, or imply two sets
-    of alligned coefficients (i.e. two TensorflowVector with
+    of aligned coefficients (i.e. two TensorflowVector with
     similar specifications).
+
+    Note that support for IndexedSlices is implemented,
+    as these are a common type for auto-differentiated
+    gradients.
+    Note that this class does not (yet?) support special
+    tensor types such as SparseTensor or RaggedTensor.
     """
 
     def __init__(
             self,
-            coefs: Dict[str, tf.Tensor]  # revise: add support for IndexedSlices?
+            coefs: Dict[str, Union[tf.Tensor, tf.IndexedSlices]]
         ) -> None:
         super().__init__(coefs)
 
@@ -36,7 +41,7 @@ class TensorflowVector(Vector):
             self,
         ) -> str:
         data = {
-            key: serialize_numpy(tns.numpy())
+            key: self._serialize_tensor(tns)
             for key, tns in self.coefs.items()
         }
         return json.dumps(data)
@@ -48,10 +53,34 @@ class TensorflowVector(Vector):
         ) -> 'TensorflowVector':
         data = json.loads(string)
         coef = {
-            key: tf.convert_to_tensor(deserialize_numpy(dat))
+            key: cls._deserialize_tensor(dat)
             for key, dat in data.items()
         }
         return cls(coef)
+
+    @classmethod
+    def _serialize_tensor(
+            cls,
+            tensor: Union[tf.Tensor, tf.IndexedSlices],
+        ) -> Tuple[Any, ...]:
+        """Convert a Tensor to a JSON-serializable object."""
+        if isinstance(tensor, tf.IndexedSlices):
+            val = cls._serialize_tensor(tensor.values)
+            ind = cls._serialize_tensor(tensor.indices)
+            return ("slices", val, ind)
+        return serialize_numpy(tensor.numpy())
+
+    @classmethod
+    def _deserialize_tensor(
+            cls,
+            data: Tuple[Any, ...],
+        ) -> Union[tf.Tensor, tf.IndexedSlices]:
+        """Re-create a Tensor from a JSON-deserialized object."""
+        if data[0] == "slices":
+            val = cls._deserialize_tensor(data[1])
+            ind = cls._deserialize_tensor(data[2])
+            return tf.IndexedSlices(val, ind)
+        return tf.convert_to_tensor(deserialize_numpy(data))  # type: ignore
 
     def __eq__(
             self,
@@ -60,9 +89,23 @@ class TensorflowVector(Vector):
         valid = isinstance(other, TensorflowVector)
         valid &= (self.coefs.keys() == other.coefs.keys())
         return valid and all(
-            np.array_equal(self.coefs[k].numpy(), other.coefs[k].numpy())
-            for k in self.coefs
+            self._tensor_equal(self.coefs[key], other.coefs[key])
+            for key in self.coefs
         )
+
+    @staticmethod
+    def _tensor_equal(
+            t_a: Union[tf.Tensor, tf.IndexedSlices],
+            t_b: Union[tf.Tensor, tf.IndexedSlices],
+        ) -> bool:
+        if not isinstance(t_a, type(t_b)):
+            return False
+        if isinstance(t_a, tf.IndexedSlices):
+            return (
+                TensorflowVector._tensor_equal(t_a.indices, t_b.indices)
+                and TensorflowVector._tensor_equal(t_a.values, t_b.values)
+            )
+        return tf.reduce_all(t_a == t_b).numpy()  # type: ignore
 
     def _apply_operation(
             self,
