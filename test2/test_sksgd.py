9bb0ef2a1c0deaa22e0d841ed8f4ddacf580f1d3
Paul Andrey <paul.andrey@inria.fr>
2022-07-29 10:26:59 +0200

    Refactored SklearnSGD tests to make them unitary.

--- test2/test_sksgd.py
+++ test2/test_sksgd.py
@@ -3,7 +3,7 @@
 """Unit tests for SklearnSGDModel."""
 
 import json
-from typing import Iterator, Optional
+from typing import List, Optional
 
 import numpy as np
 import pytest
@@ -15,12 +15,32 @@ from declearn2.model.sklearn import SklearnSGDModel
 from declearn2.typing import Batch
 
 
-def build_dataset(
+@pytest.fixture
+def model(
+        n_classes: Optional[int],
+    ) -> SklearnSGDModel:
+    """Instantiate a SklearnSGDModel to test with."""
+    skmod = (SGDClassifier if n_classes else SGDRegressor)()
+    return SklearnSGDModel(skmod, n_features=8, n_classes=n_classes)
+
+
+@pytest.fixture
+def dataset(
         n_classes: Optional[int],
         s_weights: bool,
         as_sparse: bool,
-    ) -> Iterator[Batch]:
-    """Return a random-valued dataset for a classif. or regress. task."""
+    ) -> List[Batch]:
+    """Return a random-valued dataset for a classif. or regress. task.
+
+    n_classes: int or None
+        Specify the learning task: regression if None,
+        else binary or multiclass classification.
+    s_weights: bool
+        Specify whether to use sample weights.
+    as_sparse: bool
+        Specify whether to return input features as a sparse
+        matrix object (note: it will not actually be sparse).
+    """
     rng = np.random.default_rng(seed=0)
     inputs = rng.normal(size=(2, 32, 8))
     if as_sparse:
@@ -32,56 +52,67 @@ def build_dataset(
     if s_weights:
         s_wght = np.exp(rng.normal(size=(2, 32)))
         s_wght /= s_wght.sum(axis=1, keepdims=True) * 32
-        yield from zip(inputs, labels, s_wght)
+        dataset = list(zip(inputs, labels, s_wght))
     else:
-        yield from zip(inputs, labels, [None, None])
+        dataset = list(zip(inputs, labels, [None, None]))
+    return dataset
+
+
+@pytest.mark.parametrize("n_classes", [None, 2, 5], ids=["Reg", "Bin", "Clf"])
+class TestSklearnSGDModelInit:
+    """Unit tests for declearn.model.sklearn.SklearnSGDModel.
+
+    This class groups tests that require only a model object.
+    """
+
+    def test_serialization(self, model):
+        """Check that the model can be JSON-(de)serialized properly."""
+        config = json.dumps(model.get_config())
+        other = model.from_config(json.loads(config))
+        assert model.get_config() == other.get_config()
+
+    def test_initialization(self, model):
+        """Check that weights are properly initialized to zero."""
+        w_srt = model.get_weights()
+        assert isinstance(w_srt, NumpyVector)
+        assert set(w_srt.coefs.keys()) == {'intercept', 'coef'}
+        assert all(np.all(arr == 0.) for arr in w_srt.coefs.values())
 
 
 @pytest.mark.parametrize("as_sparse", [False, True], ids=["", "Sparse"])
 @pytest.mark.parametrize("s_weights", [False, True], ids=["", "SmpWgt"])
 @pytest.mark.parametrize("n_classes", [None, 2, 5], ids=["Reg", "Bin", "Clf"])
-def test_model(
-        n_classes: Optional[int],
-        s_weights: bool,
-        as_sparse: bool,
-    ) -> None:
-    """Unit-test functionalities of a SklearnSGDModel.
+class TestSklearnSGDModelUsage:
+    """Unit tests for declearn.model.sklearn.SklearnSGDModel.
 
-    n_classes: int or None
-        Specify the learning task: regression if None,
-        else binary or multiclass classification.
-    s_weights: bool
-        Specify whether to use sample weights.
-    as_sparse: bool
-        Specify whether to return input features as a sparse
-        matrix object (note: it will not actually be sparse).
+    This class groups tests that require both a model and some data.
     """
-    # Instantiate the model and generate testing data.
-    skmod = (SGDClassifier if n_classes else SGDRegressor)()
-    model = SklearnSGDModel(skmod, n_features=8, n_classes=n_classes)
-    batches = list(build_dataset(n_classes, s_weights, as_sparse))
-    # Check that the model can be JSON-(de)serialized properly.
-    config = json.dumps(model.get_config())
-    other = model.from_config(json.loads(config))
-    assert model.get_config() == other.get_config()
-    # Check that weights are properly initialized to zero.
-    w_srt = model.get_weights()
-    assert isinstance(w_srt, NumpyVector)
-    assert set(w_srt.coefs.keys()) == {'intercept', 'coef'}
-    assert all(np.all(arr == 0.) for arr in w_srt.coefs.values())
-    # Check that gradients computation works and leaves weights unaltered.
-    grads = model.compute_batch_gradients(batches[0])
-    w_end = model.get_weights()
-    assert w_srt == w_end
-    assert isinstance(grads, NumpyVector)
-    assert grads != w_srt  # i.e. comparable but not all-zeros
-    # Test that updates' application is mathematically correct.
-    grads = -.1 * grads  # type: ignore
-    assert isinstance(grads, NumpyVector)
-    model.apply_updates(grads)
-    w_end = model.get_weights()
-    assert w_end != w_srt
-    assert w_end == (w_srt + grads)
-    # Test that loss computation abides by its specs.
-    loss = model.compute_loss(batches)
-    assert isinstance(loss, float)
+
+    def test_compute_batch_gradients(self, model, dataset):
+        """Check that gradients computation works."""
+        w_srt = model.get_weights()
+        grads = model.compute_batch_gradients(dataset[0])
+        w_end = model.get_weights()
+        assert w_srt == w_end
+        assert isinstance(grads, NumpyVector)
+        assert not all(np.all(arr == 0.) for arr in grads.coefs.values())
+        assert grads.coefs.keys() == w_srt.coefs.keys()
+
+    def test_apply_updates(self, model, dataset):
+        """Test that updates' application is mathematically correct."""
+        # Compute gradients.
+        w_srt = model.get_weights()
+        grads = model.compute_batch_gradients(dataset[0])
+        # Check that updates can be obtained and applied.
+        grads = -.1 * grads
+        assert isinstance(grads, NumpyVector)
+        model.apply_updates(grads)
+        # Verify the the updates were correctly applied.
+        w_end = model.get_weights()
+        assert w_end != w_srt
+        assert w_end == (w_srt + grads)
+
+    def test_compute_loss(self, model, dataset):
+        """Test that loss computation abides by its specs."""
+        loss = model.compute_loss(dataset)
+        assert isinstance(loss, float)
