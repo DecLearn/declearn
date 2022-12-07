0513abdd246377db96afd6ee8e8f1d2107ee71c4
Paul Andrey <paul.andrey@inria.fr>
2022-12-07 17:28:46 +0100

    Implement GradientMaskedAveraging aggregator.

--- declearn/strategy/_aggregator.py
+++ declearn/strategy/_aggregator.py
@@ -13,6 +13,7 @@ from declearn.utils import create_types_registry, register_type
 __all__ = [
     "Aggregator",
     "AverageAggregator",
+    "GradientMaskedAveraging",
 ]
 
 
@@ -158,3 +159,88 @@ class AverageAggregator(Aggregator):
             }
         total = sum(weights.values())
         return {client: weight / total for client, weight in weights.items()}
+
+
+@register_type(name="GradientMaskedAveraging", group="Aggregator")
+class GradientMaskedAveraging(AverageAggregator):
+    """Gradient Masked Averaging Aggregator subclass.
+
+    This class implements the gradient masked averaging algorithm
+    proposed and analyzed in [1] that modifies the base averaging
+    algorithm from FedAvg (and its derivatives) by correcting the
+    averaged updates' magnitude based on the share of clients that
+    agree on the updates' direction (coordinate-wise).
+
+    The formula is the following:
+        threshold in range(0, 1)  # hyperparameter
+        grads = [grads_client_0, ..., grads_client_N]
+        agree = abs(sum(sign(grads))) / len(grads)
+        score = 1 if agree >= threshold else agree
+        return score * avg(grads)
+
+    Client-based and/or number-of-training-steps-based weighting
+    may also be used, that will be taken into account both when
+    averaging input gradients and computing the coordinate-wise
+    average direction that make up for the agreement scores.
+
+    References
+    ----------
+    [1] Tenison et alii, 2022.
+        Gradient Masked Averaging for Federated Learning.
+        https://arxiv.org/abs/2201.11986
+    """
+
+    def __init__(
+        self,
+        threshold: float = 1.0,
+        steps_weighted: bool = True,
+        client_weights: Optional[Dict[str, float]] = None,
+    ) -> None:
+        """Instantiate a gradient masked averaging aggregator.
+
+        Parameters
+        ----------
+        threshold: float
+            Threshold above which to round the coordinate-wise agreement
+            score to 1. Must be in [0, 1] (FedAvg being the 0 edge case).
+        steps_weighted: bool, default=True
+            Whether to weight updates based on the number of optimization
+            steps taken by the clients (relative to one another).
+        client_weights: dict[str, float] or None, default=None
+            Optional dict of client-wise base weights to use.
+            If None, homogeneous base weights are used.
+
+        Notes
+        -----
+        * One may specify `client_weights` and use `steps_weighted=True`.
+          In that case, the product of the client's base weight and their
+          number of training steps taken will be used (and unit-normed).
+        * One may use incomplete `client_weights`. In that case, unknown-
+          clients' base weights will be set to 1.
+        """
+        self.threshold = threshold
+        super().__init__(steps_weighted, client_weights)
+
+    def get_config(
+        self,
+    ) -> Dict[str, Any]:
+        config = super().get_config()
+        config["threshold"] = self.threshold
+        return config
+
+    def aggregate(
+        self,
+        updates: Dict[str, Vector],
+        n_steps: Dict[str, int],
+    ) -> Vector:
+        # Perform gradients' averaging.
+        output = super().aggregate(updates, n_steps)
+        # Compute agreement scores as to gradients' direction.
+        g_sign = {client: grads.sign() for client, grads in updates.items()}
+        scores = super().aggregate(g_sign, n_steps)
+        scores = scores * scores.sign()
+        # Derive masking scores, using the thresholding hyper-parameter.
+        clip = (scores - self.threshold).sign().maximum(0.0)
+        scores = (1 - clip) * scores + clip  # s = 1 if s > t else s
+        # Correct outputs' magnitude and return them.
+        return scores * output
