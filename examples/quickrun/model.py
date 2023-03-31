303b90581c1db988ef15724a4382bd5d846abf13
BIGAUD Nathan <nathan.bigaud@inria.fr>
2023-03-31 12:36:13 +0200

    Heavily simplifying model

--- examples/quickrun/model.py
+++ examples/quickrun/model.py
@@ -6,12 +6,11 @@ from declearn.model.tensorflow import TensorflowModel
 
 stack = [
     tf.keras.layers.InputLayer(input_shape=(28, 28, 1)),
-    tf.keras.layers.Conv2D(32, 3, 1, activation="relu"),
-    tf.keras.layers.Conv2D(64, 3, 1, activation="relu"),
+    tf.keras.layers.Conv2D(8, 3, 1, activation="relu"),
     tf.keras.layers.MaxPool2D(2),
     tf.keras.layers.Dropout(0.25),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dense(64, activation="relu"),
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(10, activation="softmax"),
 ]
