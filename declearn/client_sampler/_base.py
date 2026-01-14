3d67113a42f7ee11eac23b8e30f6364d11ef2438
Simon Decomble <simon.decomble@inria.fr>
2026-01-14 12:03:53 +0100

    add license headers, add doc todos

--- declearn/client_sampler/_base.py
+++ declearn/client_sampler/_base.py
@@ -1,3 +1,20 @@
+# coding: utf-8
+
+# Copyright 2025 Inria (Institut National de Recherche en Informatique
+# et Automatique)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Client Sampler abstraction API"""
 
 from __future__ import annotations
@@ -365,7 +382,7 @@ class CompositionClientSampler(ClientSampler):
         Backend of the from_specs method, specific to
         'CompositionClientSampler'.
 
-        Note: each sampler composing the 'samplers' list can be either
+        Note: Each sampler composing the 'samplers' list can be either
         a dictionnary of valid sampler specification, or an instance of
         ClientSampler.
         """
