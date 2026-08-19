cb47131818677c0def2320f8d2595b765efed1af
Simon Decomble <simon.decomble@inria.fr>
2026-08-19 17:24:37 +0200

    fix(ex flamby): re-add import CIndexMetric and add ruff-ignore comment
    - this unused import was removed automatically by "ruff check --fix" but it is necessary to make the FL experiment work with this script
    - add a "noqa" comment to prevent ruff fix to remove it in the future
    - add details in a previous comment

--- examples/flamby/run_client.py
+++ examples/flamby/run_client.py
@@ -33,9 +33,10 @@ import declearn.model.torch
 from declearn.utils import config_client_loggers, make_importable
 
 # Do not remove the following "unused" import,
-# it is necessary for type registration
+# it is necessary to register and serialize this example-specific metric and
+# make the FL experiment work when using this script.
 with make_importable(os.path.dirname(__file__)):
-    pass
+    from metric import CIndexMetric  # noqa: F401
 
 import declearn
 from declearn.dataset.torch import TorchDataset
